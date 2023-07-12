cd20dc2fd60b039f3d029f3a9d2cb7056ba047cc
Kostya <ashirokovkonstantin@gmail.com>
2023-07-12 11:37:45 +0500

    Create homework 2

---
+++ Lesson_9_homework_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson_9_homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = Prompt("Введите M: ");
+            int n = Prompt("Введите N: ");
+            Console.WriteLine(Sum(m,n));
+            int Prompt(string msg)
+            {
+                Console.WriteLine(msg);
+                return int.Parse(Console.ReadLine());
+            }
+
+            int Sum(int m, int n)
+            {
+                if(m <= n) return m + Sum(m+1,n);
+                else return 0;   
+            }
+        }
+    }
+}
