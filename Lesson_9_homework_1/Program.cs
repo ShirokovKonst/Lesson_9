bb2b72dc90b368115ddeb0d337841780fc914cfe
Kostya <ashirokovkonstantin@gmail.com>
2023-07-12 10:17:34 +0500

    Create homework 1

---
+++ Lesson_9_homework_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson_9_homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Prompt("Введите число: ");
+            string result = ReverseCount(num);
+            Console.WriteLine(result.Remove(result.Length-1));
+
+            int Prompt(string msg)
+            {
+                Console.WriteLine(msg);
+                return int.Parse(Console.ReadLine());
+            }
+
+            string ReverseCount(int i)
+            {
+                if (i > 0) return i.ToString() + "," + ReverseCount(i-1);
+                else return String.Empty;
+            }
+        }
+    }
+}
